6615bcdac0abd58bdc3fb7fdbb33826bdf06ed57
kosuke.takada <ec2-user@ip-172-31-18-214.us-east-2.compute.internal>
2018-06-20 06:36:58 +0000

    first commit

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -38,5 +38,27 @@ use Illuminate\Database\Eloquent\Model;
             'keyword' => $keyword,
             'items' => $items,
         ]);
+     }
+    
+    public function show($id)
+    {
+      $item = Item::find($id);
+      $want_users = $item->want_users;
+
+      return view('items.show', [
+          'item' => $item,
+          'want_users' => $want_users,
+      ]);
+      
+      $have_users = $item->have_users;
+      
+      return view('items.show', [
+          'item' => $item,
+          'have_users' => $have_users,
+      ]);
     }
+    
+    
+      
+      
   }
